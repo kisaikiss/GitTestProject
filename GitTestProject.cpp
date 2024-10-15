df3cf3cbedc19ed50005f49708cfb31ff071088b
kobayashi masato <k024g1031@g.neec.ac.jp>
2024-10-15 10:15:54 +0900

    表示する文字を変更

--- GitTestProject.cpp
+++ GitTestProject.cpp
@@ -5,8 +5,7 @@
 
 int main() {
     std::cout << "Hello VsCode Git!!\n";
-    std::cout << "Hello New Branch!!\n";
-    std::cout << "Hello New World!!\n";
+    std::cout << "PullRequest branch changed \n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
